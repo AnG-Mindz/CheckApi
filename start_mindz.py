3c9d042ad091f2e5b1ece2be78b7e090955b9361
Gayathri Swaminathan <53771795+gaaithri@users.noreply.github.com>
2024-12-27 16:03:06 +0530

    Added Rock paper game and CORS

--- start_mindz.py
+++ start_mindz.py
@@ -3,7 +3,9 @@
 import random
 import requests
 from flask import Flask, jsonify,request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 # alphabet - a-z
 
 alpha = ['a','b','c', 'd', 'e', 'f']
@@ -38,7 +40,6 @@ def get_passwd():
         generatedpassword = generatedpassword+i
     return jsonify({"gpwd": generatedpassword})
     # Return the response from the /guess API
-    
 
 # user input : Ask user to enter the size of password they want to be generated min num 5 chatracter . 
 # then generate a random password that coinsists of 2 alphabets 2 signs and 1 number 
@@ -69,6 +70,32 @@ def start_games():
         generatedpassword = generatedpassword+i
     return jsonify({"gpwd": generatedpassword})
     # print(generatedpassword)
+
+@app.route('/gamerock', methods=["Post"])
+def play_game():
+    ''' Play Rock paper scisors with User choice'''
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+    sign_choice = [ROCK, PAPER, SCISSORS]
+    data = request.get_json()
+    user_choice= data.get("userChoice").lower()
+    if user_choice not in sign_choice :
+        return jsonify({"message":"Invalid User Choice" ,"game_code": -1, "status_code": 200})
+
+    buddy_comp_choice = random.choice(sign_choice)
+    if user_choice == buddy_comp_choice:
+        return jsonify({"message": "Its Draw", "game_code": 0,"status_code": 200,"comp_choice":""})
+    elif\
+    (user_choice == ROCK and buddy_comp_choice == SCISSORS) or \
+    (user_choice == PAPER and buddy_comp_choice == ROCK) or \
+    (user_choice == SCISSORS and buddy_comp_choice == PAPER):
+        return jsonify({"message": "You Win", "game_code": 1, "status_code": 200,\
+                         "comp_choice":buddy_comp_choice })
+    else:
+        return jsonify({"message": "Computer Won", "game_code": 2, \
+                        "status_code": 200, "comp_choice":buddy_comp_choice}) 
+
 
 
 if __name__ == '__main__':
